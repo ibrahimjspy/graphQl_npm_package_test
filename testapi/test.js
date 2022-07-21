31c97bc8d1031b8748f9ac1a93022490dd87200e
ibrahimjspy <ibrahimsajidjs@gmail.com>
2022-07-21 17:14:21 +0500

    class based npm exports support enabled .

---
+++ testapi/test.js
@@ -0,0 +1,21 @@
+// let Employee =class {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+//     getName() {
+//         console.log('hero')
+//         return this.name;
+//     }
+//     getAge() {
+//         console.log('3')
+//         return this.age;
+//     }
+// }
+ 
+// module.exports = Employee;
+
+// index.js implementation 
+
+// let Employee = require('./test.js');
+// let obj = new Employee("Isaac Tonyloi", 21);
