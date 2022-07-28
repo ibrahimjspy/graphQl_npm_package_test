8e4d851b5f0c8eb2bc0d979c5c44069675d9c71a
ibrahimjspy <ibrahimsajidjs@gmail.com>
2022-07-28 13:51:23 +0500

    Docs : adding documentation for rest methods .

--- testapi/index.js
+++ testapi/index.js
@@ -1,33 +1,53 @@
 const axiosCall = require('./fetch')
-
+/**
+ * returns default product cards for landing page 
+ */
 const getProductCards = async () => {
     return axiosCall('product/cards');
 };
-
-const getProductCardsByListId = async (id) => {
-    return axiosCall(`product/cardsByListId/${id}`);
+/**
+ * returns product cards by something id 
+ * @params you need to pass product category id
+ */
+const getProductCardsByCategoryId = async (id) => {
+    return axiosCall(`product/cardsByCategoryId/${id}`);
 };
-
+/**
+ * returns product details page data using slug
+ * @params slug should be of single product
+ */
 const getProductDetailsBySlug = async (slug) => {
     return axiosCall(`product/details/${slug}`) //product Slug
 };
-
+/**
+ * returns sections and their categories/types
+ */
 const getProductSections = async () => {
     return axiosCall('categories/productSections');
 };
-
+/**
+ * returns top menu categories , types etc
+ */
 const getMenuCategories = async () => {
     return axiosCall('categories/menu');
 };
-
+/**
+ * returns details of user dashboard based on userId
+ * @params id should be unique user id 
+ */
 const getUserDashboardById = async (id) => {
     return axiosCall(`orders/dashboardById/${id}`);
 };
-// mapped against menu categories 
+/**
+ * returns product list page with data about all types and its relating categories 
+ * @params category slug should be passed
+ */ 
 const getProductListPageBySlug = async (slug) => {
     return axiosCall(`product/list/${slug}`);
 };
-
+/**
+ * function for testing
+ */ 
 const funcTest = () => {
     console.log("I am test function from node server ");
 };
@@ -37,7 +57,7 @@ module.exports = {
     getMenuCategories,
     funcTest,
     getProductSections,
-    getProductCardsByListId,
+    getProductCardsByCategoryId,
     getProductDetailsBySlug,
     getUserDashboardById,
     getProductDetailsBySlug,
