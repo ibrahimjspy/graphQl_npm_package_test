98ff50a7c3d39de64314f0839e7f135ab8045a27
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2022-11-14 17:35:28 +0500

    feature/SHR-493:remove ip

--- testapi/module/test.js
+++
@@ -1,9 +0,0 @@
-const cart = require("./cart");
-
-const validator = async function () {
-  const cartObj = new cart();
-  const testFunc = await cartObj.getCartCheckoutBundlesByUserId("VXNlcjo0");
-  console.log(testFunc);
-};
-
-validator();
